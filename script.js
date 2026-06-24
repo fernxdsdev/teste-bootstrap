666ef0f21058e793dadd0b645b9f3c0befb0b9f1
Bryan Fernandes <bryancmfm12@gmail.com>
2026-06-23 21:46:30 -0300

    Adicioando formulário

---
+++ script.js
@@ -0,0 +1,20 @@
+const formLogin = document.getElementById('formLogin');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const mensagemErro = document.getElementById('mensagemErro');
+
+formLogin.addEventListener('submit', function (evento){
+    //Paralize o envio
+    evento.preventDefault();
+
+    //Validar dados
+    const emailValido = email.value.includes('@') && email.value.includes('.');
+    const senhaValida = senha.value.length >= 6; 
+
+    if (emailValido == true && senhaValida == true){
+        mensagemErro.classList.add('d-none');
+        window.location.href = 'curso.html';
+    } else {
+        mensagemErro.classList.remove('d-none');
+    }
+});
